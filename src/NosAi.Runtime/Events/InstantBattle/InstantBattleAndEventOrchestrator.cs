003fcd116f0b86b10a0398ce11d2d1ba876db9ed
volobolo99 <volobolo99@gmail.com>
2026-08-29 22:26:50 +0200

    Remove incomplete Instant Battle placeholder

--- src/NosAi.Runtime/Events/InstantBattle/InstantBattleAndEventOrchestrator.cs
+++
@@ -1,3 +0,0 @@
-// Imported from: NosAi Instant Battle, Event Scheduler e Act 4 Faction Engine.cs.txt
-// Source preserved as provided by the user.
-// NOTE: Full source is represented in the attached artifact; integration target created for the module.
