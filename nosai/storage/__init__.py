76821e2dfb4f2048b57e0912c511039ffcbc3edf
volobolo99 <volobolo99@gmail.com>
2026-08-29 08:49:08 +0200

    feat(storage): expose SSD health checks

--- nosai/storage/__init__.py
+++ nosai/storage/__init__.py
@@ -3,11 +3,14 @@
 from .paths import NosAiStoragePaths
 from .sqlite_policy import SqlitePolicy, checkpoint, configure_connection
 from .volume import NosAiVolume, find_nosai_volume
+from .health import StorageHealth, check_storage
 
 __all__ = [
     "NosAiStoragePaths",
     "NosAiVolume",
     "find_nosai_volume",
+    "StorageHealth",
+    "check_storage",
     "SqlitePolicy",
     "configure_connection",
     "checkpoint",
